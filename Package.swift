0c55ef291176a564c88a35c797ef87dbe07c4905
Michael Brown <michael.brown@camelotglobal.com>
2017-07-25 18:56:16 +0100

    Update package manifest to Swift 4 format.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,27 @@
-import Foundation
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "iCalendar",
+    products: [
+        .library(
+            name: "iCalendar",
+            targets: ["iCalendar"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 3)..<Version(3, .max, .max)),
-        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 7)
+        .package(url: "https://github.com/antitypical/Result.git", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "1.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "iCalendar",
+            dependencies: ["Result"],
+            path: "Sources"),
+        .testTarget(
+            name: "iCalendarTests",
+            dependencies: ["iCalendar", "Quick", "Nimble"]),
     ],
     swiftLanguageVersions: [3, 4]
 )
